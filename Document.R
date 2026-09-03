d86064ca6e56d02d8afd0d392ee0d9b94b5998b0
kamalsthapa-cmyk <kamalsthapa@gmail.com>
2026-09-03 11:07:32 -0400

    here is a change from Kamal

--- Document.R
+++ Document.R
@@ -1,7 +1,8 @@
 # Hello, this is Charles
 # Hi I am Kamal
-<<<<<<< HEAD
+
 # I am writting different code 
 =======
 # I am making a merge conflict!
->>>>>>> fb2c5b5636abc14c35e402f44d341d1e8bacc320
+
+# here is also change from Kamal
